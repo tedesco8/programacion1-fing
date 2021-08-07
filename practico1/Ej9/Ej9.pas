c7467c2f8eec74b0ae1683d309ef154fb20e0236
Pablo Tedesco <tedesco8@hotmail.com>
2021-08-06 23:14:59 -0300

    P1/Ej9: se evaluan expresiones

---
+++ practico1/Ej9/Ej9.pas
@@ -0,0 +1,12 @@
+program Ej9;
+    (*Se evaluan expresiones*)
+    begin
+        writeln(sqrt (16));
+        writeln(trunc (-13.8));
+        writeln(round (10.7));
+        writeln(sqr (5));
+        writeln(round (-3.5));
+        writeln(abs (-12));
+        writeln(trunc (10.1));
+        writeln(trunc (8.6) - round (8.6));
+    end.
