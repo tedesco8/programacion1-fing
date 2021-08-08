3b3053c9845a8338a503a6eed0986275486b1d2d
Pablo Tedesco <tedesco8@hotmail.com>
2021-08-07 21:33:25 -0300

    P1/Ej11: se determinan instrucciones de asignacion validas

---
+++ practico1/Ej11/Ej11.pas
@@ -0,0 +1,11 @@
+program Ej11;
+    var
+        num, suma, total: Integer;
+        valor: Real;
+        car1, car2: Char;
+    begin
+        num := num + num;
+        valor := total * num + suma;
+        car2 := car1;
+        valor := total;
+    end.
