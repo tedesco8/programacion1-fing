b79ebcca417e589183de77f841752f2b7bc302de
Pablo Tedesco <tedesco8@hotmail.com>
2021-08-08 16:13:01 -0300

    P1/Ej12: se determinan tipos y se calcula valor

---
+++ practico1/Ej12/Ej12.pas
@@ -0,0 +1,8 @@
+program Ej12;
+        
+    begin
+        writeln(5 + sqr(3) - 4 + trunc(3.6 - 2.1));
+        writeln(3 * sqrt(8 MOD 6 * 10 DIV 5));
+        writeln(6 + 9 * 8 DIV 2 * round(1.362) - 2 * 3);
+        writeln(trunc(12 / 5 * sqrt(4 + 4 * 3) / 4));
+    end.
