2f9b0b8f6cd36aae7fdbf4fc9d7171f1e7c9ca1e
Pablo Tedesco <tedesco8@hotmail.com>
2021-08-08 20:14:01 -0300

    P2/Ej1: se verifican valores

---
+++ practico2/Ej1/Ej1.pas
@@ -0,0 +1,17 @@
+PROGRAM Ej1 ();
+    VAR
+        a, b, c, d, x, y, z: Integer;
+    BEGIN
+        read(x,y,z); 
+        readln(a); 
+        readln(b,c); 
+        read(d);
+        
+        writeln('a: ',a);
+        writeln('b: ',b);
+        writeln('c: ',c);
+        writeln('d: ',d);
+        writeln('x: ',x);
+        writeln('y: ',y);
+        writeln('z: ',z)
+    END.
