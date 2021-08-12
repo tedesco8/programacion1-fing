eaa82d5178fc4b213d57b5687955dd043b2d7ddc
Pablo Tedesco <tedesco8@hotmail.com>
2021-08-11 21:21:05 -0300

    P2/Ej16: demostrando entrada y salida de booleanos

---
+++ practico2/Ej16/Ej16.pas
@@ -0,0 +1,11 @@
+program Ej16(input, output);
+
+var b: boolean;
+begin
+    //NO se puede leer variables booleanas desde consola.
+    // read(b)
+    b := true;
+
+    //Se puede mostrar por consola el contenido de una variable booleana.
+    write(b)
+end.
