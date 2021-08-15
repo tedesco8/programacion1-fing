01cbe01fc5cd447016beca0c3b0efc831a787833
Pablo Tedesco <tedesco8@hotmail.com>
2021-08-15 20:17:12 -0300

    P3/Ej9: escribiendo case equivalente a if

---
+++ practico3/Ej9/Ej9.pas
@@ -0,0 +1,32 @@
+(*Escriba una instrucción CASE que sea equivalente a las siguientes instrucciones IF anidadas. 
+Suponga que todas las variables son de tipo integer.
+
+ IF k = 0 THEN
+    r := r + 1
+ ELSE
+    IF k = 1 THEN
+       s := s + 1
+    ELSE
+       IF (k = 2) OR (k = 3) OR (k = 4) THEN
+          t := t + 2
+*)
+PROGRAM Ej9 (output);
+VAR k, r, s, t : Integer;
+BEGIN
+    r := 1;
+    s := 1;
+    t := 1;
+
+    writeln('Ingrese un numero del 1 al 4');
+    read(k);
+    
+    CASE k OF
+        0: r := r + 1;
+        1: s := s + 1;
+        2: t := t + 2;
+        3: t := t + 2;
+        4: t := t + 2;
+    END
+
+    // writeln('El numero ingresado: ', k, 'da como resultados: ', r,s,t );
+END.
